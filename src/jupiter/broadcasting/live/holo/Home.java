22a5b1296eb272650996818335b05a3f4b891795
TByron <sync.chromium.b@gmail.com>
2013-07-22 16:15:16 +0200

    JellyBean notification with buttons (not working)

--- src/jupiter/broadcasting/live/holo/Home.java
+++ src/jupiter/broadcasting/live/holo/Home.java
@@ -2,17 +2,20 @@ package jupiter.broadcasting.live.holo;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -35,11 +38,14 @@ public class Home extends Activity {
      */
     private final int NOTIFICATION_ID = 3434;
     MediaPlayer mp = new MediaPlayer();
+    NotificationManager mNotificationManager;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.startscreen);
         String ns = Context.NOTIFICATION_SERVICE;
+        mNotificationManager = (NotificationManager) getSystemService(ns);
+        setContentView(R.layout.startscreen);
+
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
         mNotificationManager.cancel(NOTIFICATION_ID);
         final Button play = (Button) this.findViewById(R.id.button1);
@@ -59,8 +65,7 @@ public class Home extends Activity {
                 startActivity(i);
 
             }
-        }
-        );
+        });
         Button rss = (Button) this.findViewById(R.id.button2);
         rss.setOnClickListener(new OnClickListener() {
 
@@ -78,7 +83,7 @@ public class Home extends Activity {
                     alertbox.setPositiveButton(R.string.audio, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface arg0, int arg1) {
                             try {
-                                mp.setDataSource("http://jblive.fm/");
+                                mp.setDataSource("http://jblive.am/");
                             } catch (IllegalArgumentException e) {
                                 // TODO Auto-generated catch block
                                 e.printStackTrace();
@@ -139,39 +144,20 @@ public class Home extends Activity {
             }
 
         }
-
         );
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        String ns = Context.NOTIFICATION_SERVICE;
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
         mNotificationManager.cancel(NOTIFICATION_ID);//For good measure because app pauses before it quits aswell as on pause
-
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        String ns = Context.NOTIFICATION_SERVICE;
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
         if (mp.isPlaying()) {
-            //mp.stop();
-            //mp.release();
-
-            mNotificationManager.cancel(NOTIFICATION_ID);
-            Notification notification = new Notification(R.drawable.jb_icon, "Jupiter Broadcasting", System.currentTimeMillis());
-
-            Intent notificationIntent = new Intent(this, Home.class);
-            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-            PendingIntent intent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-            notification.setLatestEventInfo(getApplicationContext(), "Jupiter Broadcasting", getString(R.string.plaiyinglivestream), intent);
-
-            notification.flags = Notification.FLAG_ONGOING_EVENT;
-            mNotificationManager.notify(NOTIFICATION_ID, notification);
+            putNotificationUp(mp.isPlaying());
         } else {
             mNotificationManager.cancel(NOTIFICATION_ID);
         }
@@ -180,14 +166,68 @@ public class Home extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        String ns = Context.NOTIFICATION_SERVICE;
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
         if (mp.isPlaying()) {
             mp.stop();
             mp.release();
         }
         mNotificationManager.cancel(NOTIFICATION_ID);//because onPause is called first
+    }
+    private void putNotificationUp(boolean play){
 
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.jb_icon)
+                        .setContentTitle("Jupiter Broadcasting")
+                        .setContentText(getString(R.string.plaiyinglivestream))
+                        .setAutoCancel(true);
+
+        Intent resultIntent = new Intent(this, Home.class);
+        // pending intent to call back the already running resultIntent
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, 0);
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        //display action buttons if 4.1+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            //Play-Pause button
+            Intent playPause = new Intent("DO_NOTIFICATION_ACTION");
+            Bundle playPauseBundle = new Bundle();
+            playPauseBundle.putInt("click", 1);//This is the value I want to pass
+            playPause.putExtras(playPauseBundle);
+            PendingIntent pendingPlayPause = PendingIntent.getActivity(this, 12345, playPause, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            String text = play ? getString(R.string.pause) : getString(R.string.play);
+            mBuilder.addAction(android.R.drawable.ic_media_pause, text, pendingPlayPause);
+
+            //Exit button
+            Intent stop = new Intent("DO_NOTIFICATION_ACTION");
+            Bundle stopBundle = new Bundle();
+            stopBundle.putInt("click", 3);//This is the value I want to pass
+            stop.putExtras(stopBundle);
+            PendingIntent pendingStop = PendingIntent.getActivity(this, 12345, stop, PendingIntent.FLAG_UPDATE_CURRENT);
+            mBuilder.addAction(android.R.drawable.ic_menu_close_clear_cancel, "Exit", pendingStop);
+
+        }
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 
+    public class NotificationReceiver extends BroadcastReceiver {
+
+       @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle answerBundle = intent.getExtras();
+            int event = answerBundle.getInt("click");
+            if (event == 1) {
+                if (mp.isPlaying()) {
+                    mp.stop();
+                    putNotificationUp(mp.isPlaying());
+               } else {
+                    mp.start();
+                    putNotificationUp(mp.isPlaying());
+                }
+            } else if (event == 3) {
+                mNotificationManager.cancel(NOTIFICATION_ID);
+                getParent().finish();
+            }
+        }
+    }
 }
